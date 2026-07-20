82cc9d68072cb922dbaf3c4036d6a89a6217d0a1
austinnikeesha <austinnikeesha@gmail.com>
2026-07-20 11:33:45 -0500

    Fix: Add flutter-gradle-plugin back with correct version (1.2.3), fix AGP to 8.7.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
@@ -48,6 +49,10 @@ android {
             )
         }
     }
+}
+
+flutter {
+    source = "../.."
 }
 
 dependencies {
