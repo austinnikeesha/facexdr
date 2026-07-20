f65f79ece9d4661643b1929e6868a4542a89af2e
austinnikeesha <austinnikeesha@gmail.com>
2026-07-20 11:43:49 -0500

    Fix: Use includeBuild for flutter-gradle-plugin from Flutter SDK

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,15 @@
 pluginManagement {
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -9,7 +20,6 @@ pluginManagement {
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
 include(":app")
