f98b687f0dc027416df5f6d0429336befd6cdecc
austinnikeesha <austinnikeesha@gmail.com>
2026-07-20 11:30:31 -0500

    Fix: Remove flutter-gradle-plugin, use standard Android/Kotlin plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -50,13 +50,8 @@ android {
     }
 }
 
-flutter {
-    source = "../.."
-}
-
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.core:core:1.13.1")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("io.flutter:flutter_embedding_release:1.0.0-83675ed27633283e7fc296c8bca22e841224c096")
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.1.0"
     id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
